797fa5f1b2fd2450177b7ad79d594ee83500f9d3
Austin Chau <auch3497@colorado.edu>
2019-10-15 20:02:23 -0600

    Enemy Attack Pattern

---
+++ Assets/EnemyChargeBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChargeBehaviour : StateMachineBehaviour
+{
+    [SerializeField]
+    [Tooltip("Amount of time charged.")]
+    private float chargeTime = .5f;
+
+    private Enemy enemy;
+    private float startTime;
+    private float timeElapsed;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Charge");
+        enemy = animator.GetComponent<Enemy>();
+        startTime = Time.time;
+        Debug.Log(startTime);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(startTime + chargeTime < Time.time)
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+}

0f767f8a56de617d7b3360b8750ae6456f2ecff3
Austin Chau <auch3497@colorado.edu>
2019-11-20 18:24:03 -0700

    Minimap

---
+++ Assets/MinimapCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 NewPos = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = NewPos;
+    }
+}
